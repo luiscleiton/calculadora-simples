e1325963103fce5e6a548de66d838048d82d1bad
luiscleiton <luiscleitonms@outlook.com>
2026-08-14 22:24:37 -0300

    Implementado função de Soma

---
+++ main.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const botaoAdicao = document.getElementById('adicao')
+
+function somar() {
+    const num1 = Number(document.getElementById('numero1').value)
+    const num2 = Number(document.getElementById('numero2').value)
+    const resultado = document.getElementById('resultado')
+
+    resultado.textContent = num1 + num2
+}
+
+botaoAdicao.onclick = somar
